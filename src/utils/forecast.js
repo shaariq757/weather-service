a36d7a7a59191520ab260fd7fdbd4e95075e2ed6
shaariq757 <shaariq725@gmail.com>
2023-06-16 09:48:18 +0530

    updated forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const foreCast = (lat,long,callback)=>{
             }
             else
             {
-                callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.')
+                callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees. Chance of rain is '+ body.current.precip)
             }
     })
 }
