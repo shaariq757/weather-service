24904210ffdaa39fbca91980463ae18248825c52
shaariq757 <shaariq725@gmail.com>
2023-07-26 20:56:20 +0530

    =split data into objects to display separately on page

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const mOne = document.querySelector('#mOne');
 const mTwo = document.querySelector('#mTwo');
+const mThree = document.querySelector('#mThree');
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
@@ -11,6 +12,7 @@ form.addEventListener('submit',(e)=>{
     const loc = input.value;
     mOne.textContent = 'Loading...'
     mTwo.textContent = ''
+    mThree.textContent = ''
     fetch('/weather?address='+loc).then((res)=>{
         res.json().then((data)=>{
             if(data.error){
@@ -18,7 +20,8 @@ form.addEventListener('submit',(e)=>{
             }
             else{
             mOne.textContent = data.location;
-            mTwo.textContent = data.forecast;
+            mTwo.textContent = data.forecast.desc;
+            mThree.textContent ='It is currently '+ data.forecast.temp +' degrees.'+'It feels like '+data.forecast.feels+'. Chances of rain is '+data.forecast.precip;
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,23 @@
-const request = require('request')
+const request = require("request");
 
+const foreCast = (lat, long, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=2da98a64ce348eecce2ccf0c90f9cea7&query=" +lat + "," + long + "&units=m";
 
-const foreCast = (lat,long,callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=2da98a64ce348eecce2ccf0c90f9cea7&query='+lat+','+long+'&units=m'
+  request({ url: url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(undefined, {
+        desc: body.current.weather_descriptions[0],
+        temp: body.current.temperature,
+        feels: body.current.feelslike,
+        precip: body.current.precip,
+      });
+    }
+  });
+};
 
-    request({url:url,json:true},(error,{body})=>{
-        if(error){
-            callback('Unable to connect weather service',undefined)
-        }
-        else if(body.error)
-        {
-        callback('Unable to find location',undefined)
-            }
-            else
-            {
-                callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees. Chance of rain is '+ body.current.precip)
-            }
-    })
-}
-
-module.exports = foreCast
+module.exports = foreCast;
